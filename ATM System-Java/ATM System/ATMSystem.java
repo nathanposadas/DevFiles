442fdd4f3fb726b60d7dd7d9d805c08d2a685cc7
Jonathan Posadas <148729777+nathanposadas@users.noreply.github.com>
2023-10-31 21:20:58 +0700

    ATM System update 1
    updated oct 31 2023
    
    binago ko yung format, meron ng balance inquiry, withdraw at deposit
    naglagyan ko narin ng random generated pin at acc number

--- ATM System-Java/ATM System/ATMSystem.java
+++ ATM System-Java/ATM System/ATMSystem.java
@@ -1,43 +1,109 @@
 import javax.swing.JOptionPane;
-import java.util.HashMap;
-
+import java.util.Random;
 public class ATMSystem {
     public static void main(String[] args) {
-        HashMap<String, String> users = new HashMap<>();
-
+        // Dito nakalagay o nakasave yung mga bagong user acc
+        String[] usernames = new String[10];
+        String[] passwords = new String[10];
+        double[] pin = new double[10];
+        double[] acc = new double[10];
+        double[] balances = new double[10];
+        int userCount = 0;
+        
+        //Yung condition ng log-in at enroll,mag enroll ka muna bago mag log in wala pang acc ito
         while (true) {
-            String[] options = {"Log In", "Enroll", "Return"};
-            int choice = JOptionPane.showOptionDialog(
-                null,
-                "Please Choose an Option:",
-                "ATM System",
-                JOptionPane.DEFAULT_OPTION,
-                JOptionPane.QUESTION_MESSAGE,
-                null,
-                options,
-                options[0]
-            );
+            String choice = JOptionPane.showInputDialog("ATM System, Welcome!"+
+                    "\nPlease Select dwe andy Option:\n1. Log In\n2. Enroll\n3. Return");
 
-            if (choice == 0) {
-                String username = JOptionPane.showInputDialog("Enter Account No.:");
-                String password = JOptionPane.showInputDialog("Enter PIN:");
+            int option = Integer.parseInt(choice);
 
-                if (users.containsKey(username) && users.get(username).equals(password)) {
-                    JOptionPane.showMessageDialog(null, "Login successful!");
-                } else {
-                    JOptionPane.showMessageDialog(null, "Login failed.");
-                }
-            } else if (choice == 1) {
-                String newUsername = JOptionPane.showInputDialog("Enter a new Account:");
-                if (users.containsKey(newUsername)) {
-                    JOptionPane.showMessageDialog(null, "Account already exists.");
-                } else {
-                    String newPassword = JOptionPane.showInputDialog("Enter a new PIN:");
-                    users.put(newUsername, newPassword);
-                    JOptionPane.showMessageDialog(null, "Enrollment successful. You can now log in.");
-                }
-            } else if (choice == 2) {
-                break;
+            switch (option) {
+                case 1:
+                    // sa log-in idol
+                    String username = JOptionPane.showInputDialog("Enter username:");
+                    String password = JOptionPane.showInputDialog("Enter password:");
+                    int userIndex = -1;
+                    
+
+                    // i checheck dito kung may naka enroll nang acc
+                    for (int i = 0; i < userCount; i++) {
+                        if (usernames[i].equals(username) && passwords[i].equals(password)) {
+                            userIndex = i;
+                            break;
+                        }
+                    }
+
+                    if (userIndex != -1) {
+                        // dito pag naka log in ka na, lalabas na din dito yung account no. at PIN random generated kada 
+                        //account
+                        while (true) {
+                            
+                            Random random = new Random();
+                            int accNum = random.nextInt(10000)+ 1000;
+                            int pinNum = random.nextInt(9000)+ 1000;
+                            String accountOption = JOptionPane.showInputDialog(
+                                    "Your PIN is: " +pinNum + "\nYour Account Number is: "+ accNum 
+                                    + "\nSelect an account option:  "
+                                    +"\n1. Balance Inquiry\n2. Withdraw\n3. Deposit\n4. Log Out");
+                            
+                            int accountChoice = Integer.parseInt(accountOption);
+                            
+                        
+                        pin[userCount] = pinNum;
+
+                            switch (accountChoice) {
+                                case 1:
+                                    JOptionPane.showMessageDialog(null, "Balance: $" + balances[userIndex]);
+                                    break;
+                                case 2:
+                                    double withdrawAmount = Double.parseDouble(JOptionPane.showInputDialog("Enter withdrawal amount:"));
+                                    if (withdrawAmount <= balances[userIndex]) {
+                                        balances[userIndex] -= withdrawAmount;
+                                        JOptionPane.showMessageDialog(null, "Withdrawn: $" + withdrawAmount);
+                                    } else {
+                                        JOptionPane.showMessageDialog(null, "Insufficient funds");
+                                    }
+                                    break;
+                                case 3:
+                                    double depositAmount = Double.parseDouble(JOptionPane.showInputDialog("Enter deposit amount:"));
+                                    balances[userIndex] += depositAmount;
+                                    JOptionPane.showMessageDialog(null, "Deposited: $" + depositAmount);
+                                    break;
+                                case 4:
+                                    JOptionPane.showMessageDialog(null, "Logged out.");
+                                    break;
+                                default:
+                                    JOptionPane.showMessageDialog(null, "Invalid account option.");
+                            }
+                        }
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Invalid username or password.");
+                    }
+                    break;
+                case 2:
+                    // Enroll
+                    if (userCount < usernames.length) {
+                        String newUsername = JOptionPane.showInputDialog("Enter a new username:");
+                        String newPassword = JOptionPane.showInputDialog("Enter a new password:");
+                        
+                        
+                        usernames[userCount] = newUsername;
+                        passwords[userCount] = newPassword;
+                        balances[userCount] = 0.0;
+                        userCount++;
+
+                        JOptionPane.showMessageDialog(null, "Enrollment successful.You can now log in.");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Maximum user limit reached. Cannot enroll more users.");
+                    }
+                    break;
+                case 3:
+                    // Return
+                    JOptionPane.showMessageDialog(null, "Exiting the program.");
+                    System.exit(0);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Invalid option.");
             }
         }
     }
